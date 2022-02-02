1e773ef41c1f3f958e9359e0c2a40149c0f8c842
Alexandr <kodak333vas@gmail.com>
2022-02-02 23:25:54 +0300

    new

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+const btnTask = document.querySelector('.btn'),
+      inputTask = document.getElementById('input');
+
+
+    let tasks = [];
+
+
+    function Task(input){
+
+        this.input=input;
+        this.condition = false;
+    }
+
+    btnTask.addEventListener('click', ()=>{
+
+       
+        
+            tasks.push(new Task(inputTask.value));
+            let clean =document.getElementById('input').value = '';
+
+            let card = document.createElement('div')
+               card.innerHTML = `
+               <div>
+               ${task.input}
+               </div>
+               
+               `       
+               
+               card.append('back_all');
+        
+        console.log(tasks);
+        
+    });
+
+
+
+    
+
+
+        
+         
+
+   
+
+
+      
